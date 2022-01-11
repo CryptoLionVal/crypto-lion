90f52ab86acb22813ba66cf82ba8c3eafd0c981c
Mevlut Canvar <mevlutcanvar@gmail.com>
2022-01-12 02:51:49 +0300

    refactors status button comp

--- components/buttons/Status.vue
+++ components/buttons/Status.vue
@@ -17,45 +17,70 @@
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    name: {
-      type: String,
-      required: true,
-    },
-    rpc: {
-      type: String,
-      required: true,
-    },
-    validator: {
-      type: String,
-      required: true,
-    },
-  },
-  data() {
-    return {
-      height: 0,
-      gap: 10,
-      missed: 0,
-      loaded: false,
-      showDesc: false,
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+type Signature = {
+  validator_address: string
+}
+
+type Block = {
+  result: {
+    block: {
+      last_commit: Array<Signature>
     }
-  },
-  computed: {
-    statusClass() {
-      if (!this.loaded) return 'animate-ping'
-      else if (this.missed === 0 && this.loaded) return 'bg-green-600'
-      else if (this.missed < 10 && this.loaded) return 'bg-yellow-600'
+  }
+}
+// commit.result.signed_header.header.height
+type Commit = {
+  result: {
+    signed_header: {
+      header: string
+    }
+  }
+}
 
-      return 'bg-red-600'
-    },
-  },
-  async mounted() {
+@Component
+export default class Status extends Vue {
+  @Prop({ required: true }) name!: string
+  @Prop({ required: true }) rpc!: string
+  @Prop({ required: true }) validator!: string
+
+  height: number = 0
+  gap: number = 10
+  missed: number = 0
+  loaded: boolean = false
+  showDesc: boolean = false
+
+  get statusClass(): string {
+    if (!this.loaded) return 'animate-ping'
+    else if (this.missed === 0 && this.loaded) return 'bg-green-600'
+    else if (this.missed < 10 && this.loaded) return 'bg-yellow-600'
+
+    return 'bg-red-600'
+  }
+
+  async checkBlock(height: number): Promise<boolean> {
+    const {
+      result: {
+        block: { last_commit: signatures },
+      },
+    }: Block = await this.$axios.$get(this.rpc + '/block?height=' + height)
+
+    return signatures.some(
+      (signature) => signature.validator_address === this.validator
+    )
+  }
+
+  async mounted(): Promise<void> {
     try {
-      const commit = await this.$axios.$get(this.rpc + '/commit')
+      const {
+        result: {
+          signed_header: { header: height },
+        },
+      }: Commit = await this.$axios.$get(this.rpc + '/commit')
 
-      this.height = parseInt(commit.result.signed_header.header.height) - 2
+      this.height = parseInt(height) - 2
 
       for (
         let height = this.height;
@@ -71,22 +96,6 @@ export default {
       // eslint-disable-next-line no-console
       console.error(e)
     }
-  },
-  methods: {
-    async checkBlock(height) {
-      const block = await this.$axios.$get(this.rpc + '/block?height=' + height)
-
-      if (
-        block.result.block.last_commit.signatures.some(
-          (signature) => signature.validator_address === this.validator
-        )
-      )
-        return true
-
-      return false
-    },
-  },
+  }
 }
 </script>
-
-<style scoped></style>
